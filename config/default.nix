6efac5a6a162e39b40dd73437e6bbba70f987d38
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2024-10-23 19:35:05 +0200

    Add some configuration

--- config/default.nix
+++ config/default.nix
@@ -18,30 +18,49 @@
   opts = {
     number = true;
     relativenumber = true;
-    list = true;
+    colorcolumn = "80,100";
     scrolloff = 8;
+
+    list = true;
+    listchars = "eol:↵,trail:~,tab:>-,nbsp:␣";
+
+    hlsearch = true;
+    hidden = true;
+    errorbells = false;
+    swapfile = false;
+    backup = false;
+    mouse = "a";
+  };
+
+  plugins = {
+    undotree.enable = true;
+    telescope.enable = true;
+    commentary.enable = true;
+    gitgutter.enable = true;
+    trim.enable = true;
+    nvim-tree.enable = true;
   };
 
   keymaps = [
-    {
-      mode = "i";
-      key = "jj";
-      action = "<Esc>";
-    }
-    {
-      mode = "n";
-      key = "<leader>w";
-      action = "<Cmd>w<CR>";
-    }
-    {
-      mode = "n";
-      key = "<leader>q";
-      action = "<Cmd>q<CR>";
-    }
-    {
-      mode = "n";
-      key = "<leader>Q";
-      action = "<Cmd>!q<CR>";
-    }
+    { mode = "n"; key = "<Up>"; action = "<Nop>"; }
+    { mode = "n"; key = "<Down>"; action = "<Nop>"; }
+    { mode = "n"; key = "<Left>"; action = "<Cmd>BufferLineCyclePrev<CR>"; }
+    { mode = "n"; key = "<Right>"; action = "<Cmd>BufferLineCycleNext<CR>"; }
+    { mode = "n"; key = "<C-h>"; action = "<Cmd>BufferLineCyclePrev<CR>"; }
+    { mode = "n"; key = "<C-l>"; action = "<Cmd>BufferLineCycleNext<CR>"; }
+    { mode = "n"; key = "<C-Left>"; action = "<Cmd>BufferLineCyclePrev<CR>"; }
+    { mode = "n"; key = "<C-Right>"; action = "<Cmd>BufferLineCycleNext<CR>"; }
+    { mode = "i"; key = "jj"; action = "<Esc>"; }
+    { mode = "n"; key = "<leader>w"; action = "<Cmd>w<CR>"; }
+    { mode = "n"; key = "<leader>q"; action = "<Cmd>q<CR>"; }
+    { mode = "n"; key = "<leader>Q"; action = "<Cmd>qall!<CR>"; }
+    { mode = "n"; key = "<leader>u"; action = "<Cmd>UndoTreeToggle<CR>"; }
+    { mode = "n"; key = "<leader>ps"; action = "<Cmd>Telescope live_grep<CR>"; }
+    { mode = "n"; key = "<leader>ff"; action = "<Cmd>Telescope find_files<CR>"; }
+    { mode = "n"; key = "<leader>d"; action = "<Cmd>bd<CR>"; }
+    { mode = "n"; key = "<leader>t"; action = "<Cmd>tabe<CR><Cmd>terminal<CR>"; }
+    { mode = "i"; key = ","; action = ",<C-g>u"; }
+    { mode = "i"; key = ";"; action = ";<C-g>u"; }
+    { mode = "n"; key = "<leader>n"; action = "<Cmd>NvimTreeToggle<CR>"; }
   ];
 }
