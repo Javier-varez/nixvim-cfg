91082823e08a178637606eef9268b7a50f282f96
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2024-10-27 23:22:43 +0100

    Manage windows

--- config/default.nix
+++ config/default.nix
@@ -1,7 +1,9 @@
+{ helpers, ... }:
 {
   # Import all your configuration modules here
   imports = [ ./bufferline.nix ];
 
+  # Tell nvim where to store the undo files
   extraConfigLuaPre = ''
     local data_dir = vim.fn.stdpath('data')
     vim.o.undodir = data_dir..'/vimdid'
@@ -11,8 +13,6 @@
     tokyonight = {
       enable = true;
       settings = {
-
-
         style = "night";
       };
     };
@@ -24,27 +24,40 @@
 
   autoCmd = [
     {
-      event = [ "BufRead" "BufNewFile" ];
-      callback = {
-        __raw = ''
+      event = [
+        "BufRead"
+        "BufNewFile"
+      ];
+      callback = helpers.mkRaw ''
         function()
+          -- Enables the undo files
           vim.bo.undofile = true
           vim.bo.expandtab = true
           vim.bo.tabstop = 4
           vim.bo.shiftwidth = 4
         end
-        '';
-      };
+      '';
     }
 
     {
-      event = [ "BufRead" "BufNewFile" ];
-      pattern = [ "*.go" "Makefile" "makefile" "*.mk" ];
+      event = [
+        "BufRead"
+        "BufNewFile"
+      ];
+      pattern = [
+        "*.go"
+        "Makefile"
+        "makefile"
+        "*.mk"
+      ];
       command = "lua vim.bo.expandtab = false";
     }
 
     {
-      event = [ "BufRead" "BufNewFile" ];
+      event = [
+        "BufRead"
+        "BufNewFile"
+      ];
       pattern = [ "COMMIT_EDITMSG" ];
       command = "vim.wo.colorcolumn = \"50,72\"";
     }
@@ -91,6 +104,10 @@
       settings.current_line_blame = true;
     };
     noice.enable = true;
+    alpha = {
+      enable = true;
+      theme = "dashboard";
+    };
 
     cmp = {
       enable = true;
@@ -162,25 +179,141 @@
   };
 
   keymaps = [
-    { mode = "n"; key = "<Up>"; action = "<Nop>"; }
-    { mode = "n"; key = "<Down>"; action = "<Nop>"; }
-    { mode = "n"; key = "<Left>"; action = "<Cmd>BufferLineCyclePrev<CR>"; }
-    { mode = "n"; key = "<Right>"; action = "<Cmd>BufferLineCycleNext<CR>"; }
-    { mode = "n"; key = "<C-h>"; action = "<Cmd>BufferLineCyclePrev<CR>"; }
-    { mode = "n"; key = "<C-l>"; action = "<Cmd>BufferLineCycleNext<CR>"; }
-    { mode = "n"; key = "<C-Left>"; action = "<Cmd>BufferLineCyclePrev<CR>"; }
-    { mode = "n"; key = "<C-Right>"; action = "<Cmd>BufferLineCycleNext<CR>"; }
-    { mode = "i"; key = "jj"; action = "<Esc>"; }
-    { mode = "n"; key = "<leader>w"; action = "<Cmd>w<CR>"; }
-    { mode = "n"; key = "<leader>q"; action = "<Cmd>q<CR>"; }
-    { mode = "n"; key = "<leader>Q"; action = "<Cmd>qall!<CR>"; }
-    { mode = "n"; key = "<leader>u"; action = "<Cmd>UndoTreeToggle<CR>"; }
-    { mode = "n"; key = "<leader>ps"; action = "<Cmd>Telescope live_grep<CR>"; }
-    { mode = "n"; key = "<leader>ff"; action = "<Cmd>Telescope find_files<CR>"; }
-    { mode = "n"; key = "<leader>d"; action = "<Cmd>bd<CR>"; }
-    { mode = "n"; key = "<leader>t"; action = "<Cmd>tabe<CR><Cmd>terminal<CR>"; }
-    { mode = "i"; key = ","; action = ",<C-g>u"; }
-    { mode = "i"; key = ";"; action = ";<C-g>u"; }
-    { mode = "n"; key = "<leader>n"; action = "<Cmd>NvimTreeToggle<CR>"; }
+    {
+      mode = "n";
+      key = "<Up>";
+      action = "<Nop>";
+    }
+    {
+      mode = "n";
+      key = "<Down>";
+      action = "<Nop>";
+    }
+    {
+      mode = "n";
+      key = "<Left>";
+      action = "<Cmd>BufferLineCyclePrev<CR>";
+    }
+    {
+      mode = "n";
+      key = "<Right>";
+      action = "<Cmd>BufferLineCycleNext<CR>";
+    }
+    {
+      mode = "n";
+      key = "<C-h>";
+      action = "<Cmd>BufferLineCyclePrev<CR>";
+    }
+    {
+      mode = "n";
+      key = "<C-l>";
+      action = "<Cmd>BufferLineCycleNext<CR>";
+    }
+    {
+      mode = "n";
+      key = "<C-Left>";
+      action = "<Cmd>BufferLineCyclePrev<CR>";
+    }
+    {
+      mode = "n";
+      key = "<C-Right>";
+      action = "<Cmd>BufferLineCycleNext<CR>";
+    }
+    {
+      mode = "i";
+      key = "jj";
+      action = "<Esc>";
+    }
+    {
+      mode = "n";
+      key = "<leader>w";
+      action = "<Cmd>w<CR>";
+    }
+    {
+      mode = "n";
+      key = "<leader>q";
+      action = "<Cmd>q<CR>";
+    }
+    {
+      mode = "n";
+      key = "<leader>Q";
+      action = "<Cmd>qall!<CR>";
+    }
+    {
+      mode = "n";
+      key = "<leader>u";
+      action = "<Cmd>UndoTreeToggle<CR>";
+    }
+    {
+      mode = "n";
+      key = "<leader>ps";
+      action = "<Cmd>Telescope live_grep<CR>";
+    }
+    {
+      mode = "n";
+      key = "<leader>ff";
+      action = "<Cmd>Telescope find_files<CR>";
+    }
+    {
+      mode = "n";
+      key = "<leader>d";
+      action = "<Cmd>bd<CR>";
+    }
+    {
+      mode = "n";
+      key = "<leader>t";
+      action = "<Cmd>tabe<CR><Cmd>terminal<CR>";
+    }
+    {
+      mode = "i";
+      key = ",";
+      action = ",<C-g>u";
+    }
+    {
+      mode = "i";
+      key = ";";
+      action = ";<C-g>u";
+    }
+    {
+      mode = "n";
+      key = "<leader>n";
+      action = "<Cmd>NvimTreeToggle<CR>";
+    }
+    # Window management
+    {
+      mode = "n";
+      key = "<A-h>";
+      action = "<C-w>h";
+    }
+    {
+      mode = "n";
+      key = "<A-j>";
+      action = "<C-w>j";
+    }
+    {
+      mode = "n";
+      key = "<A-k>";
+      action = "<C-w>k";
+    }
+    {
+      mode = "n";
+      key = "<A-l>";
+      action = "<C-w>l";
+    }
+    {
+      mode = "n";
+      key = "<A-c>";
+      action = "<C-w>c";
+    }
+    {
+      mode = "n";
+      key = "<A-v>";
+      action = "<C-w>v";
+    }
+    {
+      mode = "n";
+      key = "<A-s>";
+      action = "<C-w>s";
+    }
   ];
 }
