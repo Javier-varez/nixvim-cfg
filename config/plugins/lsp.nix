67efb0a7baede0fd867d447e761b7722053b8b72
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2024-11-03 15:58:15 +0100

    Enable lua_ls

--- config/plugins/lsp.nix
+++ config/plugins/lsp.nix
@@ -1,4 +1,4 @@
-{
+{ helpers, ... }: {
   plugins = {
     lsp = {
       enable = true;
@@ -40,6 +40,43 @@
               expr = "import <nixpkgs> {}";
             };
           };
+        };
+        lua_ls = {
+          enable = true;
+          extraOptions = helpers.mkRaw ''{
+            settings = {
+              -- Note that this table has to exist, or `on_init` will not be able to extend it.
+              Lua = {};
+            },
+            on_init = function(client, initialize_result)
+              if client.workspace_folders then
+                local path = client.workspace_folders[1].name
+                if vim.uv.fs_stat(path..'/.luarc.json') or vim.uv.fs_stat(path..'/.luarc.jsonc') then
+                  return
+                end
+              end
+
+              client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+                runtime = {
+                  -- Tell the language server which version of Lua you're using
+                  -- (most likely LuaJIT in the case of Neovim)
+                  version = 'LuaJIT'
+                },
+                -- Make the server aware of Neovim runtime files
+                workspace = {
+                  checkThirdParty = false,
+                  library = {
+                    vim.env.VIMRUNTIME
+                    -- Depending on the usage, you might want to add additional paths here.
+                    -- "$3rd/luv/library"
+                    -- "$3rd/busted/library",
+                  }
+                  -- or pull in all of 'runtimepath'. NOTE: this is a lot slower and will cause issues when working on your own configuration (see https://github.com/neovim/nvim-lspconfig/issues/3189)
+                  -- library = vim.api.nvim_get_runtime_file("", true)
+                }
+              })
+            end
+          }'';
         };
       };
     };
