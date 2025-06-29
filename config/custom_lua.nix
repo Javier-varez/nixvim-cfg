8b5e4d3b77ed1275a14db14992de2aa476cef69b
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2025-06-29 20:17:05 +0200

    Fix observed hang in rust-analyzer

--- config/custom_lua.nix
+++ config/custom_lua.nix
@@ -10,5 +10,25 @@
     vim.diagnostic.config({
       virtual_lines = true
     })
+
+    -- rust-analyzer has a tendency to scan all roots of the repository and find all relevant files.
+    -- Unfortunately this can result in an observed "hang" if a directory like `.direnv` contains
+    -- multiple gigabytes of files. This is the configuration for rustaceanvim to make sure that
+    -- `.direnv` is ignored.
+    vim.g.rustaceanvim = {
+      -- LSP configuration
+      server = {
+        default_settings = {
+          -- rust-analyzer language server configuration
+          ['rust-analyzer'] = {
+            ['files'] = {
+              ['excludeDirs'] = {
+                '.direnv'
+              },
+            },
+          },
+        },
+      },
+    }
   '';
 }
